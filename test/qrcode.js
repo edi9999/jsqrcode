5fdeee2a0c9b55607cd4198554a64c3c84927c89
Edgar HIPP <hipp.edg@gmail.com>
2017-03-28 22:55:03 +0200

    Update tests

--- test/qrcode.js
+++ test/qrcode.js
@@ -1,42 +1,40 @@
 var expect = require('chai').expect;
 var fs = require('fs');
-var PNG = require('png-js');
 var QrCode = require('../dist/index.js');
+var ImageParser = require("image-parser");
 
 it('should work with basic image', function(done) {
-
   var c = fs.readFileSync(__dirname + '/image.png');
-  var p = new PNG(c);
-
-  p.decode(function(data) {
-
+  var img = new ImageParser(c);
+  img.parse((err) => {
+    if (err) {
+      return done(err);
+    }
     var qr = new QrCode();
     qr.callback = function(result) {
-
       expect(result).to.equal('Test');
       done();
     };
-    qr.decode(p, data);
+    qr.decode({width: img.width(), height: img.height()}, img._imgBuffer);
   });
 });
 
 it('should work with imageData format', function(done) {
-
   var c = fs.readFileSync(__dirname + '/image.png');
-  var p = new PNG(c);
-
-  p.decode(function(data) {
-
+  var img = new ImageParser(c);
+  img.parse((err) => {
+    if (err) {
+      return done(err);
+    }
     var qr = new QrCode();
     qr.callback = function(result) {
-
       expect(result).to.equal('Test');
       done();
     };
     qr.decode({
-      height: p.height,
-      width: p.width,
-      data: data
+      height: img.height(),
+      width: img.width(),
+      data: img._imgBuffer
     });
   });
 });
